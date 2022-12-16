8c9977a7bb528b92109c39714550c75fec696890
junhyeok <toughhyeok96@gmail.com>
2022-12-17 03:19:34 +0900

    front end template

---
+++ front-end/web/src/components/MyContents.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1 class="mt-5">Test microservices</h1>
+                <hr>
+                <a id="brokerBtn" class="btn btn-outline-secondary" href="javascript:void(0);">Test Broker</a>
+                <a id="authBrokerBtn" class="btn btn-outline-secondary" href="javascript:void(0);">Test Auth</a>
+                <a id="logBtn" class="btn btn-outline-secondary" href="javascript:void(0);">Test Log</a>
+                <a id="mailBtn" class="btn btn-outline-secondary" href="javascript:void(0);">Test Mail</a>
+
+                <div id="output" class="mt-5" style="outline: 1px solid silver; padding: 2em;">
+                    <span class="text-muted">Output shows here...</span>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <h4 class="mt-5">Sent</h4>
+                <div class="mt-1" style="outline: 1px solid silver; padding: 2em;">
+                    <pre id="payload"><span class="text-muted">Nothing sent yet...</span></pre>
+                </div>
+            </div>
+            <div class="col">
+                <h4 class="mt-5">Received</h4>
+                <div class="mt-1" style="outline: 1px solid silver; padding: 2em;">
+                    <pre id="received"><span class="text-muted">Nothing received yet...</span></pre>
+                </div>
+            </div>
+        </div>
+    </div> 
+</template>
+
+<script>
+export default {
+  name: 'MyContents',
+}
+</script>
+
+<style>
+</style>
---
+++ front-end/web/src/components/MyFooter.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col text-center">
+                <hr>
+                <small class="text-muted">Copyright &copy; GoCode.ca</small>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MyFooter'
+}
+</script>
