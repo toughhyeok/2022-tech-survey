ec289a19b1b9afdf7b7cb971de1f275a230a269a
junhyeok <toughhyeok96@gmail.com>
2022-12-18 01:14:30 +0900

    front-end dockeriging

--- front-end/web/src/components/MyContents.vue
+++ front-end/web/src/components/MyContents.vue
@@ -2,29 +2,23 @@
   <div class="container">
         <div class="row">
             <div class="col">
-                <h1 class="mt-5">Test microservices</h1>
+                <h1 class="mt-5">Technology Trend</h1>
                 <hr>
-                <a id="brokerBtn" class="btn btn-outline-secondary" href="javascript:void(0);">Test Broker</a>
-                <a id="authBrokerBtn" class="btn btn-outline-secondary" href="javascript:void(0);">Test Auth</a>
-                <a id="logBtn" class="btn btn-outline-secondary" href="javascript:void(0);">Test Log</a>
-                <a id="mailBtn" class="btn btn-outline-secondary" href="javascript:void(0);">Test Mail</a>
-
-                <div id="output" class="mt-5" style="outline: 1px solid silver; padding: 2em;">
-                    <span class="text-muted">Output shows here...</span>
-                </div>
             </div>
         </div>
         <div class="row">
             <div class="col">
-                <h4 class="mt-5">Sent</h4>
+                <h4 class="mt-5">Programming languages</h4>
+                <hr>
                 <div class="mt-1" style="outline: 1px solid silver; padding: 2em;">
-                    <pre id="payload"><span class="text-muted">Nothing sent yet...</span></pre>
+                    // 차트
                 </div>
             </div>
             <div class="col">
-                <h4 class="mt-5">Received</h4>
+                <h4 class="mt-5">Web frameworks and technologies</h4>
+                <hr>
                 <div class="mt-1" style="outline: 1px solid silver; padding: 2em;">
-                    <pre id="received"><span class="text-muted">Nothing received yet...</span></pre>
+                    // 차트
                 </div>
             </div>
         </div>
